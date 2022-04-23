68407a5263b5aeb7ec2ed85917cf79fccf1ddbc2
Julia Azevedo <julia.azevedo@avitaseg.com.br>
2022-04-22 22:01:22 -0300

    Updating routes

--- swapp/src/App.js
+++ swapp/src/App.js
@@ -1,7 +1,13 @@
 import Navbar from './components/Navbar';
+import Router from './Router';
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="theme">
+      <Navbar />
+      <Router />
+    </div>
+  );
 }
 
 export default App;
--- swapp/src/Router.jsx
+++ swapp/src/Router.jsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Film from './pages/Film';
 import Films from './pages/Films/Films';
+import People from './pages/People/People';
 import Person from './pages/Person';
 import Subscribe from './pages/Subscribe';
 
 function Router() {
   return (
     <Routes>
-      {/* <Route path="people" element={<People />} /> */}
+      <Route path="people" element={<People />} />
       <Route path="films" element={<Films />} />
       <Route path="films/:filmId" element={<Film />} />
       <Route path="people/:personId" element={<Person />} />
       <Route path="subscribe" element={<Subscribe />} />
+      <Route path="*" element={<Navigate to="people" replace />} />
     </Routes>
   );
 }
