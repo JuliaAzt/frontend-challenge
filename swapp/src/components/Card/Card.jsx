18ce65767358e94694b5902c66d3498a54a55ad4
Julia Azevedo <julia.azevedo@avitaseg.com.br>
2022-04-22 21:48:33 -0300

    Adding subtitle to Card component

--- swapp/src/components/Card/Card.jsx
+++ swapp/src/components/Card/Card.jsx
@@ -3,12 +3,20 @@ import Typography from '../Typography';
 import './Card.scss';
 
 function Card(props) {
-  const { children, title } = props;
+  const { children, title, subtitle, className } = props;
   return (
-    <div className="card">
-      <Typography variant="title" level="medium">
-        {title}
-      </Typography>
+    <div className={`card ${className || ''}`}>
+      <div className="card__header">
+        <Typography variant="title" level="medium" className="card__title">
+          {title}
+        </Typography>
+        {subtitle !== '' && (
+          <Typography variant="text" level="small" className="card__subtitle">
+            {subtitle}
+          </Typography>
+        )}
+      </div>
+
       <div>{children}</div>
     </div>
   );
@@ -16,6 +24,8 @@ function Card(props) {
 
 Card.propTypes = {
   title: PropTypes.string,
+  subtitle: PropTypes.string,
+  className: PropTypes.string,
   children: PropTypes.any
 };
 
